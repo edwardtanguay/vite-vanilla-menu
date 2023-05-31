e0d0c2687bde66740a86d016e9927c27fd86df7e
Edward Tanguay (windows) <edward@tanguay.info>
2023-05-31 19:59:29 +0200

    menu styled

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import './style.scss';
+import * as tools from './tools';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
 <h1>Info Site</h1>
 <nav>
 	<ul>
-		<li>Welcome</li>
-		<li>Info</li>
-		<li>About</li>
+		<li><a href="welcome" class="active">Welcome</a></li>
+		<li><a href="info">Info</a></li>
+		<li><a href="about">About</a></li>
 	</ul>
 </nav>
 <p>Welcome to this site.</p>
+<p>${tools.getCurrentUrl()}</p>
 `;
