57fbae8fb0d25ff8ab117d13effb4c81b561b729
Edward Tanguay (windows) <edward@tanguay.info>
2023-05-31 19:43:33 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './style.scss';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML =/* html */`
+<h1>Info Site</h1>
+<nav>
+	<ul>
+		<li>Welcome</li>
+		<li>Info</li>
+		<li>About</li>
+	</ul>
+</nav>
+<p>Welcome to this site.</p>
+`;
