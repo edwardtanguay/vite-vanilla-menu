bfdc5519007145f2964fde68af43b92749f16b7f
Regina <ritschi25@gmx.de>
2023-07-04 15:19:35 +0200

    projekte-added

--- src/pages/PageProjekte.ts
+++ src/pages/PageProjekte.ts
@@ -1,7 +1,53 @@
 export const PageProjekte = () => {
 	return /*html*/`
 <div class="page pageProjekte">
-	<p>This is the Projekte Site</p>
+	
+<article id="eispalast">
+<h2>Eispalast, nach Disneys "Eiskönigin"</h2>
+
+<p>Den Eispalast habe ich Weihnachtsgeschenk für meine Tochter bei Ali-Express bestellt und ca. 1 &frac12; Monate später erhalten. Ich hatte zeitig bestellt und es kam zum Glück noch zwei Wochen vor Weihnachten an. </p>
+
+<p>Ich habe mich für diesen Palast entschieden und nicht für das Lego Original, da dieser Eispalast mehr halbdurchsichtige Bausteine nutzt und auch gleich Beleuchtungselemente dabei waren. Wenn man nicht unter Zeitdruck ist, kann man auch immer auf Coupons oder Sales-Aktionen warten und bekommt das Set noch günstiger, in meinem Fall für 86€ inkl. Versand. So ein Preis-Leistungs-Verhältnis ist kaum zu toppen.</p>
+
+<p>Das Pake kam gut verpackt an, ohne Originalkarton, aber dafür mit gedruckter Anleitung im Softcover-Format.</p>
+
+<p>Die 2247 Teile waren in einzelnen, mit Zahlen (1-5) versehenen, Tüten verpackt, das hat den Vorteil, dass man nicht alle Tüten gleichzeitig aufreißen muss, sondern nur die Zahl, die grade in der Anleitung gefordert wird.</p>
+
+<p>Das Schloss ließ sich relativ gut aufbauen, dürfte aber im Großen und Ganzen immer noch ca. 24 h gedauert haben, inklusive Kinder und Katze von den losen Steinen fernhalten.</p>
+
+<p>Wir begannen mit dem Fundament, alles schön sortiert, bereitgelegt, und los ging es...</p>
+<figure>
+	<img src="./images/eispalast_1.jpg" alt="Eispalast Fundament" title="Eispalast Fundament">
+</figure>
+
+<p>Ich empfehle aber jedem bei diesem Set ein paar weiße 6er oder 8er Steine extra zu besorgen, um das Fundament in der Mitte zu verstärken, anderenfalls wird es schwer, wenn man den Eispalast aufbaut, das einem nicht alles zusammenkracht beim Bespielen.</p>
+
+<figure>
+	<img src="./images/eispalast_2.jpg" alt="Eispalast halbfertig" title="Eispalast halbfertig">
+</figure>
+
+<p>Halbzeit: Die erste Hälfte des Gesamtschlosses steht. Die heiße “Bitte-nicht-jetzt-schon-spielen” -Phase begann.</p>
+
+<figure>
+	<img src="./images/eispalast_3.jpg" alt="Eispalast fertig" title="Eispalast fertig">
+	<img src="./images/eispalast_4.jpg" alt="Eispalast fertig" title="Eispalast fertig">
+	<img src="./images/eispalast_5.jpg" alt="Eispalast fertig" title="Eispalast fertig">
+</figure>
+
+<p>Fertig! Jetzt kann es losgehen. Mama hatte Spaß und das Töchterchen ist begeistert. Also alles richtig gemacht. Der Imposante Eispalast kann sich aber auch sehen lassen. </p>
+
+<figure>
+	<img src="./images/eispalast_6.jpg" alt="Eispalast bespielt" title="Eispalast bespielt">
+</figure>
+<p><span>Fazit:</span></p>
+<p>Ein bomben Projekt, zu einem relativ günstigen Preis.</p>
+
+</article>
+
+<section class="fixed-top">
+<p><a href="#top">nach Oben</a></p>
+</section>
+
 </div>
 `; 
 }
