97b8d4a49dfbcd950cdc75a7ade879dc662d97a9
Regina <ritschi25@gmx.de>
2023-07-04 15:16:40 +0200

    lego-impressum-added

--- src/pages/PageLego.ts
+++ src/pages/PageLego.ts
@@ -1,7 +1,37 @@
 export const PageLego = () => {
 	return /*html*/`
 <div class="page pageLego">
-	<p>This is the Lego page.</p>
+	
+<article>
+    <section>
+    <h2>Allgemein</h2>
+    
+        <p>Die Lego A/S (auch The LEGO Group, Eigenschreibweise: LEGO) ist ein dänisches Unternehmen mit Sitz in Billund und gehört zu den drei größten Spielwarenherstellern der Welt nach Erlös und ist 2020 der größte nach Umsatz. Das Unternehmen wurde durch die mittlerweile als Spielzeugklassiker geltenden Legosteine bekannt, bei denen es sich im Wesentlichen um ein Baukastensystem handelt, in dem Häuser, Fahrzeuge und anderes aus farbigen Kunststoff-Klemmbausteinen und -Steckbauteilen gebaut werden können. Es werden vor allem Bausätze verkauft, die man zu vorgegebenen oder selbsterfundenen Modellen zusammensetzen kann. Das Wort Lego leitet sich von dänisch leg godt („spiel gut“) ab.</p>
+    </section>
+
+    <section>
+    <h2>Geschichte</h2>
+        <p>1949 wurden Bausteine eingeführt, die den heutigen Steinen bereits ähnelten. Anfänglich nannte Christiansen seine Spielsteine „Automatic Binding Bricks“. Es handelte sich um farbige Kunststoff-Quader aus Celluloseacetat, deren Oberseiten, wie bei den heutigen Steinen, mit Noppen besetzt waren. Die Unterseite war jedoch völlig hohl, was dazu führte, dass mit diesen Steinen gebaute Modelle nicht besonders stabil waren. Dieses Problem wurde beseitigt, indem in die Unterseite der Steine Röhren integriert wurden. Das ermöglichte außerdem, dass die Steine auch seitlich versetzt aufeinander gesteckt werden konnten. Dieses Kupplungsprinzip wurde am 28. Januar 1958 zum Patent angemeldet, das 1978 auslief.</p>
+
+        <figure>
+            <img src="./images/lego_logo.png" alt="altes Lego Logo" title="altes Lego Logo">
+        </figure>
+
+        <p>Das britische Unternehmen Kiddicraft, gegründet 1932 von Hilary Page, hatte prinzipiell baugleiche Spielzeugsteine aus Kunststoff bereits ab Ende der 1930er-Jahre produziert. Lego übernahm das Design, nachdem es Muster des englischen Fabrikats erhielt. Da Kiddicraft nur das Patentrecht in Großbritannien erworben hatte, konnte Lego das Produkt international vertreiben. Nach dem Tod von Hilary Page 1957 liefen dessen Patente in Großbritannien bald aus; Lego erwarb 1981 alle Patentrechte des Unternehmens Kiddicraft.</p>
+        
+        <p>Von 1955 bis 1968 produzierte Lego – ab 1958 auch in einem Werk im schleswig-holsteinischen Hohenwestedt – Modellfahrzeuge aus ABS-Kunststoff mit Rädern und Achsen aus Metall. Die Modelle wurden realen Vorbildern im Maßstab 1:87 (Nenngröße H0) nachempfunden. Die Serien umfassten 13 PKW- und drei VW-Transporter-Modelle in transparenten Garagen, zwei verschiedene Lastwagen-Typen (Bedford A und Mercedes-Benz Kurzhauber) mit verschiedenen Aufbauten, sowie Zubehör wie Zweiräder, Tankstelle, Verkehrsinsel, Bäume, Straßenleuchten und Verkehrszeichen, die in den letzten Jahren auf die Lego-Noppen steckbar waren.Passende Gebäude konnten aus den bereits vertriebenen Bausteinen gebaut werden, ergänzbar durch weitere Elemente wie Garagentor und spezieller Grundplatte.</p>
+        
+        <p>Ab 1996 war der Legostein vom Deutschen Patent- und Markenamt als Marke eingetragen. Dagegen gingen mehrere Wettbewerber vor. Am 16. Juli 2009 bestätigte der Bundesgerichtshof in letzter Instanz die vom Bundespatentgericht ausgesprochene Löschung der Marke. Im Interesse der Allgemeinheit müssten Formen vom Markenschutz freigehalten werden, deren wesentliche Merkmale eine technische Funktion erfüllten. Die Noppen auf der Oberseite des Spielsteins seien Teil des für Lego typischen Klemmsystems und hätten damit ausschließlich eine technische Funktion, urteilte der Bundesgerichtshof. Entsprechend urteilte am 14. September 2010 der Europäische Gerichtshof.</p>
+    </section>
+
+
+<section>
+    <p><span>Für weitere Informationen und Anregungen:</span></p>
+    <p><a href="https://www.lego.com/de-de">&#10149; LEGO</a></p>
+    <p><a href="https://de.wikipedia.org/wiki/Lego">&#10149; Wikipedia</a></p>
+</section>
+</article>
+
 </div>
 `;
 }
