91fdd1b2c451d41e15eac00d581a8d1ecf0cfa82
Regina <ritschi25@gmx.de>
2023-07-04 15:12:58 +0200

    willkommen-eingefuegt

--- src/pages/PageWillkommen.ts
+++ src/pages/PageWillkommen.ts
@@ -1,7 +1,44 @@
 export const PageWillkommen = () => {
 	return /*html*/`
 <div class="page pageWillkommen">
-	<p>This is the Willkommen page.</p>
+	
+<article>
+    <h2>Wilkommen auf 1001 Klemmbausteine</h2>
+    <p>Schön, dass ihr den Weg hierher gefunden habt. Ich habe diese Seite erstellt, da es doch recht wenig Erfahrungsberichte zu Klemmbausteinen gibt, die nicht dem Hause Lego entsprigen.</p>
+    <p>Ich möchte hier mit euch meine Projekte teilen, euch Klemmbaustein-Alternativen zeigen und die jeweiligen Vor- und Nachteile aufzeigen.</p>
+</article>
+
+<section class="start">
+    <a href="./pages/lego.html">
+    <h2>Lego</h2>
+    <figure>
+        <img src="./images/lego_logo.png" alt="Lego" title="Lego">
+        </figcaption>
+    </figure>
+    <p>Lego ist der Weltweite Marktführer für Klemmbausteine.</p>
+</a>
+</section>
+
+<section class="start">
+    <a href="./pages/alternativen.html">
+    <h2>Lego-Set-Alternativen</h2>
+    <figure>
+        <img src="./images/mould_king.png" alt="Mould King Truck" title="Mould King Truck">
+    </figure>
+    <p>Es gibgt verschiedene Klemmbaustein-Hersteller und auch Anbieter von MOCs mit Nutzung alternativer Herstller</p>
+</a>
+</section>
+
+<section class="start">
+    <a href="./pages/projekte.html">
+    <h2>Projekte</h2>
+    <figure>
+        <img src="./images/eispalast_4.jpg" alt="Eispalast" title="Eispalast">
+    </figure>
+    <p>Eines der aufegbauten Sets war ein Eispalast angelehnt an Disneys Eiskönigin.</p>
+</a>
+</section>
+
 </div>
 `; 
 }
