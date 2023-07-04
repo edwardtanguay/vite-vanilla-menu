fac3a9757c6226896602145c3e5ff3eba8bdf268
Regina <ritschi25@gmx.de>
2023-07-04 15:18:13 +0200

    alternativen-added

--- src/pages/PageAlternativen.ts
+++ src/pages/PageAlternativen.ts
@@ -1,7 +1,89 @@
 export const PageAlternativen = () => {
 	return /*html*/`
 <div class="page pageAlternativen">
-	<p>This is the Alternativen Site</p>
+	
+<nav2>
+<ul>
+	<li><a href="#mould_king">Mould King</a></li>
+	<li><a href="#qman">Qman</a></li>
+	<li><a href="#gobricks">GoBricks</a></li>
+</ul>
+</nav2>
+
+<article id="mould_king">
+<h2>Mould King</h2>
+
+<p>Mould King ist eine Tochtergesellschaft der ehemals Lepin-Marke, heute King. Die Produktpalette ist sehr vielfältig. Empfohlen werden die Sets bereits für Kinder, es finden sich aber auch viele Pakete, die vor allem ältere Bauer ansprechen würden. Die Bausteine sollen laut Website mit denen von Lego kompatibel sein, sodass man beide Marken auch miteinander verbinden kann.</p>
+<p>Lego und Mould King sehen auf den ersten Blick sehr ähnlich aus. Beide Marken verzeichnen eine große und vielfältige Produktpalette und sprechen ein breites Publikum an. Hinzu kommt, dass die Bausteine miteinander kompatibel sind, sodass man sich eigentlich nicht zwischen den beiden entscheiden muss.</p>
+<p>Mould King nutzt weitestgehend Steine des Herstellers GoBricks</p>
+<figure>
+	<img src="./images/mould_king.png" alt="Mould King Truck" title="Mould King Truck">
+</figure>
+<p>Ein wesentlicher Faktor, den Sie beim Kauf von Mould King bedenken sollten, ist, dass die Mutterfirma Lepin offenbar schon oftmals mit Lego angeeckt ist. Der Grund dafür ist, dass Lepin offenbar immer wieder Modelle von Lego kopiert. Es gab hier bereits ein strafrechtliches Verfahren, welches Lego gewann. Ob das auch auf die Produkte von Mould King zutrifft, ist fraglich.</p>
+<p>Der Grund, warum es überhaupt zu solchen Rechtsstreits kommen konnte, war, dass Lego seine Patentrechte auf den typische Legobaustein 2010 verlor. Dadurch ist es anderen Herstellern möglich, ähnliche Bausteine zu verkaufen. Dies gilt aber nicht für Figuren, designte Modelle oder Sets - Hier greift weiterhin das Urheberrecht, dass das geistige Eigentum und die Ideen der Lego-Designer schützt.</p>
+<p>Beide Marken haben eine gute Qualität. Das ist in diversen Rezensionen und Erfahrungsberichten ersichtlich. Unterschiede finden sich ganz klar beim Preis: Dies lässt sich etwa bei einem Geländewagen-Set sehr schön sehen. Beide Modelle von Lego und Mould King sehen sehr ähnlich aus und haben auch etwa gleich viele Teile. Der von Lego kostet allerdings fast das Doppelte.</p>
+<p>Fazit: Wenn Sie eine günstige und qualitativ adäquate Alternative zu Lego suchen, werden Sie auch mit Mould King glücklich. Vor allem, wenn Sie nach aufwendigeren Projekten suchen oder mit Robotik spielen möchten, könnte Ihnen Mould King gefallen. Für Kinder und bei farbenfrohen, lockeren Sets empfehlen wir eher Lego.</p>
+<p><a href="https://praxistipps.chip.de/mould-king-und-lego-das-muessen-sie-wissen_131680" target="_blank">&#10149; Original Artikel auf chip.de</a></p>
+</article>
+
+<article id="qman">
+<h2>Qman</h2>
+
+<p>Qman, kurz für Guangdong Qman Toys Industry Co., Ltd. ist ein chinesischer Hersteller von Spielzeug aus Kunststoff, insbesondere Klemmbausteinen.</p>
+
+<section>
+<h3>Geschichte</h3>
+<p>Qman ging nach eigenen Angaben aus der 1984 gegründeten Shantou Chenghai Huaxin Plastic Moulding Factory hervor. Der Unternehmer Zhan Kehua hatte von einer Auslandsreise Sets von Klemmbausteinen nach China mitgebracht. Er beobachtete das große Interesse seiner Kinder an dem Spielzeug und beschloss 1994, mit Qman eine chinesische Klemmbausteinmarke zu entwickeln.Bis 2019 wurden die Qman-Bausteine unter der Marke Enlighten vertrieben. Aufsehen erregte im Jahr 2004 eine rechtliche Auseinandersetzung mit dem Marktführer Lego, der nach einem gewonnenen Prozess die vom finnischen Zoll beschlagnahmte und für den russischen Markt vorgesehene Ware von 54.514 Qman-Sets durch eine Planierraupe vernichten ließ.</p>
+<p>Enlighten vertrieb vorrangig eins-zu-eins-Kopien von Lego-Sets und ging dabei soweit die eigenen Packungen aus Fotokopien der originalen Legoverpackungen herzustellen, auf denen lediglich das LEGO Logo mit dem eigenen ersetzt wurde. Noch heute tritt das Unternehmen unter dem geänderten Namen Qman mit dem Slogan „enlighten your dreams“ auf. Im außereuropäischen Raum werden nach wie vor eins-zu-eins-Kopien von Lego-Sets unter der Marke Enlighten vertrieben.</p>
+<p>Nach Angaben des Unternehmens wurde 2017 die Marke von 15 Milliarden verkauften Klemmbausteinen im Jahr überschritten. Qman gehört heute im asiatischen Raum zu den größten Herstellern von Konstruktionsspielzeug.</p>
+</section>
+
+<figure>
+	<img src="./images/qman.png" alt="Qman Feuerwehr" title="Qman Feuerwehr">
+</figure>
+
+<section>
+<h3>Produkt</h3>
+<p>Qman vertreibt hauptsächlich Klemmbausteine mit Noppen im 8mm-Format, die „exakt kompatibel zu Legos Original sind“. Die Firma bietet eine Vielzahl von Sets in verschiedenen Themenwelten an. Ähnlich wie der polnische Hersteller Cobi führt auch Qman ein verhältnismäßig großes Sortiment militärischer Baukästen.</p>
+</section>
+
+<section>
+<h3>Rezeption</h3>
+<p>Einem breiteren europäischen Publikum wurde Qman im Jahr 2021 bekannt, nachdem das Unternehmen Lego wegen potentieller Markenrechtsverletzung einen Container mit Qman-Produkten durch den Zoll beschlagnahmen ließ und juristisch gegen den europäischen Qman-Generalimporteur Thorsten Klahold vorging. Die Auseinandersetzung erzeugte ein breites Medienecho. Mehrere reichweitenstarke Webvideoproduzenten wie z. B. Gronkh schalteten sich in den Konflikt ein. Dabei wurde zumeist die Darstellungen von Klahold übernommen, dass Klahold in Form der Steingemachtes GmbH und Qman unbescholtene Firmen seien, die zu Unrecht unter Generalverdacht gestellt würden als Teil unfairer Praktiken von LEGO Konkurrenz vom europäischen Markt fernzuhalten.</p>
+<p>Wenig Beachtung fand jedoch, dass Klahold auf seinem geschäftlich genutzten YouTube Kanal „Johnny's World“ zuvor immer wieder Plagiate chinesische Hersteller wie Lepin vorgestellt und Tipps gegeben hatte wie man diese am besten durch den deutschen Zoll bekomme. Laut der Internetseite stonewars.de wurden Inhalte auf dem Kanal zur Beschaffung von Produktfälschungen nach einem Interview mit Thorsten Klahold gelöscht. Ebenfalls größtenteils unerwähnt blieb, dass sich hinter der beim Zoll zurückgehaltenen Klemmbausteinmarke Qman das gleiche Unternehmen verbirgt, welches bis 2019 unter dem Namen Enlighten in großem Umfang eins-zu-eins-Kopien von Lego-Sets vertrieb und damit ein ähnliches Geschäftsmodell betrieb wie der Hersteller Lepin, der sowohl in Europa als auch China wegen Produktfälschung verurteilt wurde.</p>
+<p>Die Produkte von Qman gelten nach Ansicht von Thomas Panke und Qman-Generalimporteur Thorsten Klahold als qualitativ hochwertig und seien denen des Marktführers Lego ebenbürtig. Lego hingegen macht die Verletzung ihrer Rechte in Bezug auf Qman Minifiguren geltend und zweifelt an der Unbedenklichkeit der Kunststoffbausteine im Sinne europäischer Richtlinien in Bezug auf Schadstoffbegrenzungen in Kunststoffen.</p>
+<p><a href="https://de.wikipedia.org/wiki/Qman" target="_blank">&#10149; Lies den ganzen Artikel auf Wikipedia</a></p>
+</section>
+</article>
+
+<article id="gobricks">
+<h2>GoBricks</h2>
+
+<p>Seit geraumer Zeit wandert ein chinesischer Hersteller durch die Internetforen, Facebook und Reddit. GoBricks <a href="https://gobricks.cn" target="_blank">(https://gobricks.cn)</a> ist der dort oft gelobte Hersteller von Klemmbausteinen aus Fernost.</p>
+
+<p>Der Grund für den „Hype“ um Klemmbausteinen von GoBricks ist erst auf den zweiten Blick erkennbar. Im Grunde muss man wissen, dass in der Vergangenheit und zum Teil auch noch heute, die Klemmbausteine aus asiatischer Eigenproduktion – denn auch Lego stellt seit Jahren Steine in China her – nicht immer der gewünschten Qualität der europäischen Käufer entsprochen haben. Die so genannte Klemmkraft war teils, wenn auch zuweilen nur auf bestimmte Teile beschränkt, unterirdisch, sodass man die Sets nicht wirklich gut aufbauen konnte. Auch war die Farbtreue einer Farbe in einem Set zuweilen nicht gegeben, sodass Wände oder auch andere eigentlich homogene Flächen fleckig aussahen.</p>
+
+<figure>
+	<img src="./images/gobricks.jpeg" alt="GoBricks Logo" title="GoBricks Logo">
+</figure>
+
+<p>Erstmals kam dann mit der Marke GoBricks ein Player in den Markt, der sich selber hohe Qualitätsansprüche gesetzt hatte. Mit der eigenen Marke Mould King, die in allen Bereichen interessante Sets im Angebot hatte und hat, war auch schnell ein gewisser Bekanntheitsgrad erreicht.</p>
+
+<p><span>Wer oder was ist GoBricks?</span></p>
+<p>Hinter GoBricks steckt die Shantou Gaodes Precision Technology Co., Ltd. und wurde 2017 gegründet. Es befindet sich in der Industriezone Dutou, Bezirk Chenghai, Stadt Shantou, Provinz Guangdong, „Chinese Toy Gift City“.</p>
+
+<p><span>Wer nutzt nun Steine von GoBricks und in welchem Umfang?</span></p>
+<p>Mould King, Sembo, Super 18K, Rael und Xinyu verwendeten ausschließlich Gobricks für über 90 % ihres Teilebedarfs. Zu beachten ist dabei, dass Decool Gobricks über seine Untermarke JiSi (teilweise) verwendet. Im Moment ist die Belieferung von Großkunden wie Mold King das Hauptgeschäft von Gobricks.</p>
+
+<p><a href="https://brickreview.de/?p=78"target="_blank">&#10149; Der ganze Artikel auf brickreview.de</a></p>
+
+
+</article>
+
+<section class="fixed-top">
+<p><a href="#top">nach Oben</a></p>
+</section>
+
 </div>
 `; 
 }
