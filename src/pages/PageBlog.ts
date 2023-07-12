aa58fbfe7222683344a6f690c554ec97786ee51b
Regina <ritschi25@gmx.de>
2023-07-12 13:34:15 +0200

    added-blogsite-with-vitepress-using-iframe

--- src/pages/PageBlog.ts
+++ src/pages/PageBlog.ts
@@ -2,6 +2,8 @@ export const PageBlog = () => {
 	return /*html*/`
 <div class="page pageBlog">
 	<p>This is the Blog Site</p>
+	<iframe src="klemmbaustein-blog/docs/index.md" frameborder="0" width="100%" height="auto></iframe>
+
 </div>
 `; 
 }
